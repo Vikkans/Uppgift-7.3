d6a73adf4684ec7daefe088c8019252c696eb26f
Vikkans <143171920+Vikkans@users.noreply.github.com>
2024-02-09 12:43:13 +0100

    Add project files.

---
+++ Uppgift 7.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> size = new Dictionary<string, int>();
+        size["Sverige"] = 450;
+        size["Norge"] = 385;
+        size["Danmark"] = 43;
+        size["Finland"] = 338;
+        
+
+        Console.WriteLine("Välj ett av länderna Sverige, Norge, Danmark, Finland");
+        string svar = Console.ReadLine();
+
+        Console.WriteLine(size[svar]);
+    }
+}
